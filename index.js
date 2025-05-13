b8fa11abc41e5337c8fd266dd573aaf4ff59e352
Vadim Tofan <vad.tofan@gmail.com>
2025-05-13 14:35:37 +0200

    Changed the syntax of the if statements for improved readability

--- index.js
+++ index.js
@@ -25,17 +25,13 @@ app.get("/past-meals", async (request, response) => {
 
 app.get("/first-meal", async (request, response) => {
   const meal = await getMeals("WHERE `id` = (SELECT MIN(`id`) FROM meal)");
-  if (!meal || (Array.isArray(meal) && meal.length === 0)) {
-    return response.send("There are no meals for your request!");
-  }
+  if (!meal || (Array.isArray(meal) && meal.length === 0)) return response.send("There are no meals for your request!");
   response.send(meal);
 });
 
 app.get("/last-meal", async (request, response) => {
   const meal = await getMeals("WHERE `id` = (SELECT MAX(`id`) FROM meal)");
-  if (!meal || (Array.isArray(meal) && meal.length === 0)) {
-    return response.send("There are no meals for your request!");
-  }
+  if (!meal || (Array.isArray(meal) && meal.length === 0)) return response.send("There are no meals for your request!");
   response.send(meal);
 });
 
